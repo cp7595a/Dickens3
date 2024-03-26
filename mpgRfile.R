73086f5ca1a9343895db39dd5d493286020a080f
Cristina Perez <112970743+cp7595a@users.noreply.github.com>
2024-03-26 18:04:47 -0400

    Update mpgRfile.R
    Cristina Perez

--- mpgRfile.R
+++ mpgRfile.R
@@ -1,5 +1,11 @@
-# J Dickens  James Dickens
+# C Perez  Cristina Perez
 
+# The data source is the mpg data table
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+
+ggplot(mpg, aes(x = "", y = displ)) +
+  geom_boxplot(fill = 'blue') +
+  coord_flip() +
+  ylab("Displacement")
